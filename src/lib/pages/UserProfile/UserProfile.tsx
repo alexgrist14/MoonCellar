b549eccf78f4bdf0a5c26a50a95b7dcc1a15927d
alexgrist14 <alexgrist133@gmail.com>
2024-10-25 14:33:35 +0300

    add label to input

--- src/lib/pages/UserProfile/UserProfile.tsx
+++ src/lib/pages/UserProfile/UserProfile.tsx
@@ -9,18 +9,21 @@ interface UserProfileProps {
 }
 
 const UserProfile: FC<UserProfileProps> = ({ email, name }) => {
-  const [isPictureLarge, setIsPictureLagre] = useState<boolean>(false);
+  const [isPictureLarge, setIsPictureLarge] = useState<boolean>(false);
   const [profileHover,setProfileHover] = useState<boolean>(false);
   const handleInput = (event: ChangeEvent<HTMLInputElement>) => {
     if (event.target.files !== null) {
+      const file = event.target.files[0];
       const fileSize = Math.round(event.target.files[0].size / 1024);
-      if (fileSize > 1024) setIsPictureLagre(true);
-    } else setIsPictureLagre(false);
+      console.log(file)
+      if (fileSize > 1024) setIsPictureLarge(true);
+    } else setIsPictureLarge(false);
   };
   return (
     <div className={styles.container}>
       <div className={styles.content}>
-        <div className={styles.profile_image} onMouseOut={()=>{setProfileHover(false)}} onMouseOver={()=>{setProfileHover(true)}}>
+        <label htmlFor="avatar">
+          <div className={styles.profile_image} onMouseOut={()=>{setProfileHover(false)}} onMouseOver={()=>{setProfileHover(true)}}>
           <Image
             src="/images/user.png"
             width={150}
@@ -31,7 +34,8 @@ const UserProfile: FC<UserProfileProps> = ({ email, name }) => {
           </div>
           <SvgCamera className={`${styles.svg} ${profileHover && styles.hover_svg}`} />
         </div>
-        <input type="file" onChange={handleInput} />
+        <input type="file" id="avatar" hidden onChange={handleInput} />
+        </label>
         <div className={styles.profile_info}>
           <div className={styles.profile_name}>{name}</div>
           <div className={styles.profile_email}>{email}</div>
