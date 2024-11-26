cc276d9c13ac58d8460f2475237b6f6704da0312
SergeyHist <sergeyhist@yahoo.com>
2024-11-26 13:46:45 +0300

    fixes

--- src/lib/shared/ui/Navigation/Navigation.tsx
+++ src/lib/shared/ui/Navigation/Navigation.tsx
@@ -1,28 +1,20 @@
 import { FC } from "react";
 import styles from "./Navigation.module.scss";
 import { links } from "../../constants";
-import Link from "next/link";
-import { Button } from "../Button";
 import { useCommonStore } from "../../store/common.store";
+import { ButtonGroup } from "../Button/ButtonGroup";
 
 export const Navigation: FC = () => {
   const { setExpanded } = useCommonStore();
 
   return (
-    <div className={styles.nav}>
-      {links.map((link, i) => (
-        <Link
-          key={i}
-          href={link.link}
-          onClick={() => {
-            setExpanded("none");
-          }}
-        >
-          <Button color="default" className={styles.nav__button}>
-            {link.name}
-          </Button>
-        </Link>
-      ))}
-    </div>
+    <ButtonGroup
+      wrapperClassName={styles.nav}
+      buttons={links.map((link) => ({
+        title: link.name,
+        link: link.link,
+        callback: () => setExpanded("none"),
+      }))}
+    />
   );
 };
