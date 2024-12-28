c89a0ad2e232862578f7e1e460bd0acdcf162d89
alexgrist14 <alexgrist133@gmail.com>
2024-12-28 14:38:20 +0300

    fix: resolve conflicts

--- src/lib/pages/UserProfile/UserInfo/UserInfo.tsx
+++ src/lib/pages/UserProfile/UserInfo/UserInfo.tsx
@@ -12,36 +12,54 @@ import styles from "./UserInfo.module.scss";
 import { userListCategories } from "@/src/lib/shared/constants/user.const";
 import { API_URL, FRONT_URL, getImageLink } from "@/src/lib/shared/constants";
 import { commonUtils } from "@/src/lib/shared/utils/common";
-
+import { Button } from "@/src/lib/shared/ui/Button";
+import { useAuthStore } from "@/src/lib/shared/store/auth.store";
+import { mergeLogs } from "@/src/lib/shared/utils/logs";
 
 interface UserInfoProps {
   userName: string;
   _id: string;
-  games: UserGamesType;
+  //games: UserGamesType;
   avatar?: string;
   logs: ILogs[];
 }
 
 const UserInfo: FC<UserInfoProps> = ({
-  games,
+ // games,
   userName,
   _id: id,
   avatar,
   logs,
 }) => {
+  const { profile } = useAuthStore();
+
+  //const mergedLogs = mergeLogs(logs);
+
   const [userFollowings, setUserFollowing] = useState<
     IFollowings | undefined
   >();
 
-  console.log(commonUtils.getHumanDate("2024-12-11T13:46:52.373+00:00"));
+  const [follow, setFollow] = useState(
+    profile?.followings.includes(id) ? "Unfollow" : "Follow"
+  );
 
-  const setActivityType = (action:string, isAdd: boolean, rating?: number) =>{
-    if(rating){
-      return `${isAdd ? `Set rating to ${rating}` : `Removed rating`}`;
-    } else {
-      return `${isAdd ? `Added to ${action}` : `Removed from ${action}`}`;
-    }
-  }
+  const setActivityType = (action: string, isAdd: boolean, rating?: number) => {
+    const actions = action.split(" and ");
+    const result = actions.map((act) => {
+      if (act === "rating") {
+        return `${isAdd ? `set rating ${rating}` : `removed rating`}`;
+      } else {
+        return `${isAdd ? `added to ${act}` : `removed from ${act}`}`;
+      }
+    });
+    return commonUtils.upFL(result.join(" and "));
+  };
+
+  useEffect(() => {
+    if (profile?.followings.includes(id)) {
+      setFollow("Unfollow");
+    } else setFollow("Follow");
+  }, [id, profile?.followings]);
 
   useEffect(() => {
     setUserFollowing(undefined);
@@ -52,11 +70,21 @@ const UserInfo: FC<UserInfoProps> = ({
       .catch();
   }, [id]);
 
+  const handleFollowClick = () => {
+    if (profile?.followings.includes(id)) {
+      userAPI.removeUserFollowing(profile._id, id);
+      setFollow("Follow");
+    } else if (profile) {
+      userAPI.addUserFollowing(profile._id, id);
+      setFollow("Unfollow");
+    }
+  };
+
   return (
     <>
       <div className={styles.content__top}>
-        <div className={styles.profile_info}>
-          <div className={styles.profile_image}>
+        <div className={styles.profile__info}>
+          <div className={styles.profile__image}>
             <Image
               key={id}
               src={avatar || "/images/user.png"}
@@ -65,12 +93,18 @@ const UserInfo: FC<UserInfoProps> = ({
               alt="profile"
               className={styles.image}
             />
+            {id !== profile?._id && (
+              <Button className={styles.btn} onClick={handleFollowClick}>
+                {follow}
+              </Button>
+            )}
           </div>
+
           <div>
-            <div className={styles.profile_name}>{userName}</div>
-            <div className={styles.profile_stats}>
-              <div className={styles.profile_stats__list}>
-                {userListCategories.map((category, i) => (
+            <div className={styles.profile__name}>{userName}</div>
+            <div className={styles.profile__stats}>
+              <div className={styles.profile__stats__list}>
+                {/* {userListCategories.map((category, i) => (
                   <Link
                     href={`${FRONT_URL}/user/${userName}?list=${category}`}
                     key={i}
@@ -79,7 +113,7 @@ const UserInfo: FC<UserInfoProps> = ({
                       games[category].length
                     }`}</span>
                   </Link>
-                ))}
+                ))} */}
               </div>
             </div>
           </div>
@@ -101,32 +135,37 @@ const UserInfo: FC<UserInfoProps> = ({
         </div>
       </div>
       <div className={styles.content__bottom}>
-        <div className={styles.activity}>
-          <h3 className={styles.activity__title}>Activity</h3>
-          <div className={styles.activity__list}>
-            {logs.map((log, i) => (
-              <div className={styles.item} key={i}>
-                <Link href={`/games/${log.game.slug}`} className={styles.item__img} target="_blank">
-                    <Image width={80} height={120} src={getImageLink(log.game.cover.url, "cover_small") } alt="cover"/>
-                </Link>
-                <div className={styles.item__text}>
-                  <p>{log.game.name}</p>
-                  <p>
-                    {
-                      setActivityType(log.action, log.isAdd, log?.rating)
-                    }
-
-                  </p>
-                  <p className={styles.date}>
-                  {
-                    commonUtils.getHumanDate(log.date)
-                  }
-                  </p>
+        {logs.length > 0 && (
+          <div className={styles.activity}>
+            <h3 className={styles.activity__title}>Activity</h3>
+            <div className={styles.activity__list}>
+              {logs.map((log, i) => (
+                <div className={styles.item} key={i}>
+                  <Link
+                    href={`/games/${log.game.slug}`}
+                    className={styles.item__img}
+                    target="_blank"
+                  >
+                    <Image
+                      width={70}
+                      height={110}
+                      src={getImageLink(log.game.cover.url, "cover_big")}
+                      style={{ width: "80px", height: "120px" }}
+                      alt="cover"
+                    />
+                  </Link>
+                  <div className={styles.item__text}>
+                    <p>{log.game.name}</p>
+                    <p>{setActivityType(log.action, log.isAdd, log?.rating)}</p>
+                    <p className={styles.date}>
+                      {commonUtils.getHumanDate(log.date)}
+                    </p>
+                  </div>
                 </div>
-              </div>
-            ))}
+              ))}
+            </div>
           </div>
-        </div>
+        )}
       </div>
     </>
   );
