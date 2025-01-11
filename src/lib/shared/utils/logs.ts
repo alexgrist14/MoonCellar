7cd6438c76e50ff79f6cdd1123ab3f81ec50ed99
SergeyHist <sergeyhist@yahoo.com>
2025-01-11 18:03:13 +0300

    fix

--- src/lib/shared/utils/logs.ts
+++ src/lib/shared/utils/logs.ts
@@ -8,23 +8,20 @@ export const removeDuplicateLogs = (logs: ILogs[]) => {
     if (!acc.length) {
       acc.push(curr);
     } else if (
-      lastParent?.action !== "rating" &&
-      curr.action !== "rating" &&
+      last.gameId === curr.gameId &&
       lastParent?.gameId === curr.gameId
     ) {
-      acc.splice(1, 1, curr);
-    } else if (lastParent?.action === "rating" && curr.action === "rating") {
-      acc.splice(1, 1, curr);
-    } else if (last.action === "rating" && curr.action !== "rating") {
-      acc.splice(0, 0, curr);
-    } else if (last.action === "rating" && curr.action === "rating") {
-      acc.splice(0, 1, curr);
-    } else if (
-      last.action !== "rating" &&
-      curr.action !== "rating" &&
-      last.gameId === curr.gameId
-    ) {
-      acc.splice(0, 1, curr);
+      if (
+        (lastParent?.action !== "rating" && curr.action !== "rating") ||
+        (lastParent?.action === "rating" && curr.action === "rating")
+      ) {
+        acc.splice(1, 1, curr);
+      }
+    } else if (last.gameId === curr.gameId) {
+      (last.action === "rating" && curr.action === "rating") ||
+      (last.action !== "rating" && curr.action !== "rating")
+        ? acc.splice(0, 1, curr)
+        : acc.splice(0, 0, curr);
     } else {
       acc.splice(0, 0, curr);
     }
@@ -37,13 +34,13 @@ export const mergeLogs = (logs: ILogs[]) => {
   return removeDuplicateLogs(logs).reduce<ILogs[]>((prev, curr) => {
     const lastLog = prev[prev.length - 1];
 
-    if (lastLog && lastLog.gameId == curr.gameId) {
+    if (lastLog?.gameId == curr.gameId && lastLog.isAdd === curr.isAdd) {
       lastLog.action = `${lastLog.action} and ${curr.action}`;
       if (curr.rating) lastLog.rating = curr.rating;
       return prev;
     }
 
-    prev.push({ ...curr });
+    prev.push(curr);
     return prev;
   }, []);
 };
