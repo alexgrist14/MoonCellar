452fcba5c1fd78e898527d83ca74bde1b24ff02f
SergeyHist <sergeyhist@yahoo.com>
2025-06-19 12:50:36 +0300

    fix: disable react scan

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -54,7 +54,7 @@ export default async function App({ children }: { children: ReactNode }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/images/mooncellar.ico" />
       </head>
-      <ReactScan />
+      {/* <ReactScan /> */}
       <body>
         <Layout
           className={classNames(general.variable, pentagra.variable)}
