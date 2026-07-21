9a648421622b983c074db66a6bf1ae26eb78cab9
SergeyHist <sergeyhist@yahoo.com>
2026-07-21 15:13:57 +0300

    feat: change wheel color generation

--- src/lib/shared/utils/wheel.utils.ts
+++ src/lib/shared/utils/wheel.utils.ts
@@ -2,14 +2,12 @@ export const generateWheelColors = (
   count: number,
   hue: number = (200 + Math.random() * 30) ^ 0
 ): string[] => {
-  const min = 10;
   const saturation = "60%";
+  const lightnessA = "30%";
+  const lightnessB = "50%";
 
   return Array.from({ length: count }, (_, i) => {
-    const percent =
-      i <= count / 2 ? (70 / count) * i : (70 / count) * (count - i + 1);
-
-    const lightness = (percent > min ? percent : min) + "%";
+    const lightness = i % 2 === 0 ? lightnessA : lightnessB;
 
     return `hsl(${hue}, ${saturation}, ${lightness})`;
   });
